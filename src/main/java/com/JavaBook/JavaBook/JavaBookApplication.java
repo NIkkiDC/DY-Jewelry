007c430eea7033c15b3ff7e28ecc7a494c3e1387
Dominique <DCWTLT1@gmail.com>
2023-04-27 17:19:57 -0500

    first push

---
+++ src/main/java/com/JavaBook/JavaBook/JavaBookApplication.java
@@ -0,0 +1,13 @@
+package com.JavaBook.JavaBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JavaBook/JavaBook/JavaBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaBook.JavaBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
