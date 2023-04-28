b3d89e41c6c519e60b3f8fd15e62eb332e11f175
Dominique <DCWTLT1@gmail.com>
2023-04-28 16:11:25 -0500

    creating the categories table

--- src/main/java/com/JavaBook/JavaBook/model/Category.java
+++ src/main/java/com/JavaBook/JavaBook/model/Category.java
@@ -1,5 +1,23 @@
 package com.JavaBook.JavaBook.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categories") // SQL table name
+
 public class Category {
 
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
