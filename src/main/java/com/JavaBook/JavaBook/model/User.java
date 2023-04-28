e547fff232b4944397087768bddd40b6405f908e
Dominique <DCWTLT1@gmail.com>
2023-04-28 16:13:47 -0500

    creating the users table

--- src/main/java/com/JavaBook/JavaBook/model/User.java
+++ src/main/java/com/JavaBook/JavaBook/model/User.java
@@ -1,4 +1,23 @@
 package com.JavaBook.JavaBook.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+
+    @Id // id attributes below. Once You enter @Entity / @Table w/o an id attribute an error will be thrown
+    // the error will go away once you create the id attributes, which are below.
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
