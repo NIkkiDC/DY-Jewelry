1435aaceb89e45fb5c33bfec4ad0abe33c1808df
Dominique <DCWTLT1@gmail.com>
2023-04-28 19:37:17 -0500

    This isn't going to work just yet bc I haven't started an attribute

--- src/main/java/com/JavaBook/JavaBook/model/Jewelry.java
+++ src/main/java/com/JavaBook/JavaBook/model/Jewelry.java
@@ -1,10 +1,9 @@
 package com.JavaBook.JavaBook.model;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Jewelry") // SQL table name
@@ -25,7 +24,14 @@ public class Jewelry {
     private String stone;  //metal type, stone type, and price range
 
     @Column
-    private Integer price;
+    private Integer price;  //metal type, stone type, and price range
+
+
+
+    @JsonIgnore //used to ignore this property during serialization and deserialization
+    @ManyToMany //this entity has a many-to-many relationship with another entity.
+    @JoinColumn(name = "category_id") // the column in this entity will map to the foreign key column in the join table
+    private Category category;
 
 
 
