310675891e56976d2b15b8ad80fa21f665c9ac53
Dominique <DCWTLT1@gmail.com>
2023-04-28 21:36:18 -0500

    returning user list

--- src/main/java/com/JavaBook/JavaBook/model/User.java
+++ src/main/java/com/JavaBook/JavaBook/model/User.java
@@ -1,8 +1,6 @@
 package com.JavaBook.JavaBook.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
@@ -11,7 +9,20 @@ public class User {
 
     @Id // id attributes below. Once You enter @Entity / @Table w/o an id attribute an error will be thrown
     // the error will go away once you create the id attributes, which are below.
+    @Column
+    @GeneratedValue
     private Long id;
+
+    private String userName;
+
+
+    public User() {
+    }
+
+    public User(Long id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
 
     public void setId(Long id) {
         this.id = id;
