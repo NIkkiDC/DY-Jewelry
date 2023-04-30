c0a2dc495a508049067c5a1900f850eaaf8fbbf0
Dominique <DCWTLT1@gmail.com>
2023-04-30 16:03:26 -0500

    updating the commentary for creating the instances

--- src/main/java/com/JavaBook/JavaBook/model/LoginRequest.java
+++ src/main/java/com/JavaBook/JavaBook/model/LoginRequest.java
@@ -1,5 +1,12 @@
 package com.JavaBook.JavaBook.model;
 
+
+/**
+ * creating two instance variables email & password
+ * create a getter & a return for both (no setter)
+ * The email or the password cannot be changed once the objects have been created
+ */
+
 public class LoginRequest {
     private String email;
     private String password;
