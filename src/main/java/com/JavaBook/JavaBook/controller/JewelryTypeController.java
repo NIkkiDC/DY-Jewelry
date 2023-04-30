a9d3652b7951fb84651ca31c64e923362fc21322
Dominique <DCWTLT1@gmail.com>
2023-04-30 11:30:36 -0500

    adding the @Autowired

--- src/main/java/com/JavaBook/JavaBook/controller/JewelryTypeController.java
+++ src/main/java/com/JavaBook/JavaBook/controller/JewelryTypeController.java
@@ -1,10 +1,13 @@
 package com.JavaBook.JavaBook.controller;
 
 
+import com.JavaBook.JavaBook.model.Jewelry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 
 /*
@@ -21,13 +24,13 @@ private JewelryTypeController jewelryTypeController; //declaring a private insta
     when this method is called it is setting the value of jewelryTypeController (used in dependency)
      */
 
-
+    @Autowired
     public void setJewelryTypeController(JewelryTypeController jewelryTypeController){this.jewelryTypeController = jewelryTypeController;}
 
 
     @GetMapping(path = "/jewelryTest/") //http://localhost:9091/jewelryTest/
     public String jewelryTest() {return "Jewelry Test";}
 
-
-
+    @GetMapping(path = "/controller/") //http://localhost:9091/controller/
+    public List<Jewelry> getJewelry() {return jewelryTypeController.getJewelry();}
 }
