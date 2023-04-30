e68eb19a6a0a0bbdcd8c7e9761e28ee6e1f703ae
Dominique <DCWTLT1@gmail.com>
2023-04-30 11:01:14 -0500

    updating essential comments about defining the rest api endpoint

--- src/main/java/com/JavaBook/JavaBook/controller/JewelryTypeController.java
+++ src/main/java/com/JavaBook/JavaBook/controller/JewelryTypeController.java
@@ -4,6 +4,11 @@ package com.JavaBook.JavaBook.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+/*
+Defining the REST API endpoint for jewelry types that can be accessed through the http request (the url)
+ */
 @RestController
 @RequestMapping(path = "restapi") //http://localhost:9091/testing
 public class JewelryTypeController {
