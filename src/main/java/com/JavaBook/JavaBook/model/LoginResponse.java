ac522ab551f7ee71dc1496221be022c76e3996fa
Dominique <DCWTLT1@gmail.com>
2023-04-30 16:23:55 -0500

    creating a constructor that takes a string   creating a class(LoginResponse) that will take the parameter response   that will then set the value as response   creating a getter method for the String response

--- src/main/java/com/JavaBook/JavaBook/model/LoginResponse.java
+++ src/main/java/com/JavaBook/JavaBook/model/LoginResponse.java
@@ -1,6 +1,21 @@
 package com.JavaBook.JavaBook.model;
 
+
+/**
+ * creating a constructor that takes a string
+ * creating a class(LoginResponse) that will take the parameter response
+ * that will then set the value as response
+ * creating a getter method for the String response
+ *
+ */
 public class LoginResponse {
+private String response;
+
+public LoginResponse(String response) {this.response = response;}
+
+    public String getResponse() {return response;}
+
+    public void setResponse(String response) {this.response = response;}
 }
 
 
