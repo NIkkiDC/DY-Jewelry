b63201ae7580dfc5d8d08246d612e85cbd5cdb4f
Dominique <DCWTLT1@gmail.com>
2023-04-30 10:57:58 -0500

    declaring a private instance variable

--- src/main/java/com/JavaBook/JavaBook/controller/JewelryTypeController.java
+++ src/main/java/com/JavaBook/JavaBook/controller/JewelryTypeController.java
@@ -1,7 +1,13 @@
 package com.JavaBook.JavaBook.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "restapi") //http://localhost:9091/testing
 public class JewelryTypeController {
-private JewelryTypeController jewelryTypeController;
+private JewelryTypeController jewelryTypeController; //declaring a private instance variable jewelryTypeController of type JewelryTypeController.
 
 
 }
