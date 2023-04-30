0ec9b4d36804eb6b4400576cf29a8bc7147c8318
Dominique <DCWTLT1@gmail.com>
2023-04-30 15:45:22 -0500

    integrate the user functionality

--- src/main/java/com/JavaBook/JavaBook/controller/UserController.java
+++ src/main/java/com/JavaBook/JavaBook/controller/UserController.java
@@ -1,15 +1,27 @@
 package com.JavaBook.JavaBook.controller;
 
 import com.JavaBook.JavaBook.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequestMapping(path = "/auth/users")
 public class UserController {
 
     private UserController userController;
 
-
+    @Autowired
     public void setUserController(UserController userController) {this.userController = userController;}
 
+
+    @PostMapping("/register")
     public User createUser(@RequestBody User userObject) {return userController.createUser(userObject);}
+
+
 
 }
