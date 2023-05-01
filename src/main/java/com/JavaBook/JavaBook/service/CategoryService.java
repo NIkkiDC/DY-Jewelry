ffe2882f3942e687af96530f9d39ebf62935554c
Dominique <DCWTLT1@gmail.com>
2023-05-01 08:19:13 -0500

    jewelry type repo

--- src/main/java/com/JavaBook/JavaBook/service/CategoryService.java
+++ src/main/java/com/JavaBook/JavaBook/service/CategoryService.java
@@ -1,5 +1,24 @@
 package com.JavaBook.JavaBook.service;
 
-public class CategoryService {
-}
 
+import com.JavaBook.JavaBook.model.JewelryType;
+import com.JavaBook.JavaBook.repository.JewelryRepository;
+import com.JavaBook.JavaBook.repository.JewelryTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    private JewelryRepository jewelryRepository;
+    private JewelryTypeRepository jewelryTypeRepository;
+
+    @Autowired
+    public void setCategoryRepository(JewelryTypeRepository jewelryTypeRepository) {
+        this.jewelryTypeRepository = jewelryTypeRepository;
+    }
