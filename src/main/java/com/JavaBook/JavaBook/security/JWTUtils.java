06cf97a57eaf28fe4b1103f6f4d6af81b33c4c4c
Dominique <DCWTLT1@gmail.com>
2023-04-30 21:35:27 -0500

    updating security configuration

---
+++ src/main/java/com/JavaBook/JavaBook/security/JWTUtils.java
@@ -0,0 +1,12 @@
+package com.JavaBook.JavaBook.security;
+
+public class JWTUtils {
+    public boolean validateJwtToken(String jwt) {
+
+        return false;
+    }
+
+    public String getUserNameFromJwtToken(String jwt) {
+        return jwt;
+    }
+}
--- src/main/java/com/JavaBook/JavaBook/security/JwtUtils.java
+++
@@ -1,4 +0,0 @@
-package com.JavaBook.JavaBook.security;
-
-public class JwtUtils {
-}
