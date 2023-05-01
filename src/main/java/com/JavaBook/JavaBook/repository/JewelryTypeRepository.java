a25004d5643e8854e4eaf2f921d08dedb9d2dfd7
Dominique <DCWTLT1@gmail.com>
2023-04-30 22:06:11 -0500

    updating the extends JpaRepository

--- src/main/java/com/JavaBook/JavaBook/repository/JewelryTypeRepository.java
+++ src/main/java/com/JavaBook/JavaBook/repository/JewelryTypeRepository.java
@@ -3,6 +3,9 @@ package com.JavaBook.JavaBook.repository;
 import com.JavaBook.JavaBook.model.JewelryType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JewelryTypeRepository extends JpaRepository<JewelryType, Long> {
+    List<JewelryType> findByCategoryId(Long categoryId);
 }
 
