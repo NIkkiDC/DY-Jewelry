82fd4bad5678eadaa6648210773a844e4f67d205
Dominique <DCWTLT1@gmail.com>
2023-04-30 21:47:56 -0500

    updating user repository interfaces

--- src/main/java/com/JavaBook/JavaBook/repository/UserRepository.java
+++ src/main/java/com/JavaBook/JavaBook/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.JavaBook.JavaBook.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // register
+
+
 }
