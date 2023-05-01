196a1523d1a4cf89e95b1d262f843a36c8055797
Dominique <DCWTLT1@gmail.com>
2023-04-30 22:10:21 -0500

    updating Jewelry repository file to input the extends JpaRepository

--- src/main/java/com/JavaBook/JavaBook/repository/JewelryRepository.java
+++ src/main/java/com/JavaBook/JavaBook/repository/JewelryRepository.java
@@ -4,8 +4,12 @@ package com.JavaBook.JavaBook.repository;
 import com.JavaBook.JavaBook.model.Jewelry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JewelryRepository extends JpaRepository<Jewelry, Long> {
     // select name from categories where name =
     Jewelry findByName(String name);
+
+    List<Jewelry> findByUserId(Long userId);
 }
 
