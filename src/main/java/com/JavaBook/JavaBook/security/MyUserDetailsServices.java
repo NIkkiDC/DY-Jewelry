ed4bb6cb1d125e8459a9a9da5286f7ef461d833d
Dominique <DCWTLT1@gmail.com>
2023-04-30 21:31:10 -0500

    user details services

--- src/main/java/com/JavaBook/JavaBook/security/MyUserDetailsServices.java
+++ src/main/java/com/JavaBook/JavaBook/security/MyUserDetailsServices.java
@@ -1,4 +1,26 @@
 package com.JavaBook.JavaBook.security;
 
-public class MyUserDetailsServices {
+import com.JavaBook.JavaBook.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email); // jay@jay.com
+        return new MyUserDetails(user);
+    }
 }
--- src/main/java/com/JavaBook/JavaBook/service/UserService.java
+++ src/main/java/com/JavaBook/JavaBook/service/UserService.java
@@ -1,4 +1,10 @@
 package com.JavaBook.JavaBook.service;
 
+import org.springframework.security.core.userdetails.User;
+
 public class UserService {
+    public User findUserByEmailAddress(String email) {
+
+        return null;
+    }
 }
